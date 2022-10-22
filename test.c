f11b2dd37fa8a9e3f79108c53513aa7e316e840c
mouseVi <dr.kpbic@gmail.com>
2022-10-22 16:55:23 +0300

    Testing -- amend ) change commit + add and remove file and repeat

--- test.c
+++ test.c
@@ -1,6 +1,5 @@
-branch\main test 1
-branch\main test 2
-branch\main test 3
-branch\main test 4
-branch\main test 5
-branch\main main
+AAAAAAAAAAAAAAAAAAA
+cltkfk bprfreg w 
+wg wcstombsrth w 
+wk woh 
+wh wcstombsrh wh
